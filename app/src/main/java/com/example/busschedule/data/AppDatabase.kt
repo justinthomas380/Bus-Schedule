b468bcb1dc85125b33946d59e0f7db05195ef702
Justin Thomas <84940076+justinthomas380@users.noreply.github.com>
2024-04-05 16:58:02 -0400

    Initial commit with few comments mostly notes TODO(1)

---
+++ app/src/main/java/com/example/busschedule/data/AppDatabase.kt
@@ -0,0 +1,48 @@
+package com.example.busschedule.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+/**NOTES
+ * [BusSchedule::class] is an array literal and can only be used in annotations.
+ * Outside annotations you must use arrayOf() or specific functions like intArrayOf()
+ * ExportSchema set to false so as not to keep a schema backup history.
+ **/
+@Database(entities = [BusSchedule::class], version = 1, exportSchema = false)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun busScheduleDao(): BusScheduleDao
+
+    /**NOTES
+     * Companion objects are like the static keyword in java
+     * Being declared within a class companion objects can be accessed
+     * without needing to create an instance of the class.
+     **/
+    companion object{
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            /**
+             * Code wrapped in a synchronized block can only execute on one
+             * thread at a time.
+             * In this function the database only gets initialized once.
+             **/
+            return INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    AppDatabase::class.java,
+                    "app_database"
+                )
+                    .createFromAsset("database/bus_schedule.db")
+                    //TODO("Try to code migration before submitting")
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also{
+                        INSTANCE = it
+                    }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/busschedule/ui/BusScheduleViewModel.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.busschedule.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.busschedule.BusScheduleApplication
+import com.example.busschedule.data.BusSchedule
+import com.example.busschedule.data.BusScheduleDao
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+class BusScheduleViewModel(
+    private val busScheduleDao: BusScheduleDao): ViewModel() {
+
+    /** Gets the full schedule from the room in ASC order.
+     * NOTES
+     * Flow<List<BusSchedule>> is the return type and is inferred by kotlin.
+     * This is added for readability and the return type is ran against the getAll()
+     * function return type at compile time to ensure compatibility.
+     * Since this is compared at compile time there is not runtime overhead.
+     */
+    fun getFullSchedule() = busScheduleDao.getAll()
+
+    /** Gets the schedule by stop name in ASC order.
+     * NOTES
+     * Flow<List<BusSchedule>> is the return type and is inferred by kotlin.
+     * This is added for readability and the return type is ran against the getByStopName()
+     * functions return type at compile time to ensure compatibility.
+     * Since this is compared at compile time there is not runtime overhead.
+     */
+    fun getScheduleFor(stopName: String): Flow<List<BusSchedule>> = busScheduleDao.getByStopName(stopName)
+
+    companion object {
+        val factory : ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as BusScheduleApplication)
+                BusScheduleViewModel(application.database.busScheduleDao())
+            }
+        }
+    }
+}
+
+
